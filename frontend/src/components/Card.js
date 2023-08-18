6d9bc4d5993576cfadfc146ec62fec0d4e90ddaa
Maria Fetisova <racusmaria@gmail.com>
2023-08-19 00:04:40 +0300

    feature: добавить cors и краш-тест

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -17,7 +17,7 @@ function Card(props) {
 
   const currentUser = React.useContext(CurrentUserContext);
   const isOwn = props.card.owner === currentUser._id;
-  const isLiked = props.card.likes.some(i => i._id === currentUser._id);
+  const isLiked = props.card.likes.some(i => i === currentUser._id);
   const cardLikeButtonClassName = (`element__button-like ${isLiked && 'element__button-like_active'}`);
 
   return (
